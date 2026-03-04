ba25454c94575f23782a4edd3aba7a462695db4e
RomanForDev <romanfortinidev@gmail.com>
2026-03-04 15:21:55 -0300

    Mongoose instalado y creado archivo con modelos (products.model.js)

---
+++ src/models/products.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const productCollection = 'products';
+
+const productSchema = new mongoose.Schema({
+    _id: {
+        type: String, 
+        required: true, 
+        unique: true
+    },
+    name: {
+        type: String, 
+        required: true, 
+        unique: true
+    },
+    price: {
+        type: Number, 
+        required: true
+    },
+    status: Boolean,
+    quantity: Number,
+    description: {
+        type: String, 
+        required: true
+    }
+})
+
+export const productModel = mongoose.model(productCollection, productSchema);
